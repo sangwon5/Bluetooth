b639cc63a625bd358c5f3e8a1ded11dd5d5de459
sangwon OH <ntts7***@gmail.com>
2021-05-27 06:13:42 +0900

    add beacon list (prints out UUID and Distance add beacon Ranging mode

--- app/src/main/java/com/example/bluetooth/beacon.java
+++ app/src/main/java/com/example/bluetooth/beacon.java
@@ -1,8 +1,12 @@
 package com.example.bluetooth;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -10,56 +14,99 @@ import android.os.Message;
 import android.os.RemoteException;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
-import org.altbeacon.beacon.AltBeacon;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.estimote.sdk.SystemRequirementsChecker;
+
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
-import org.altbeacon.beacon.Identifier;
 import org.altbeacon.beacon.MonitorNotifier;
-import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
-import java.util.jar.Manifest;
-
-import com.estimote.sdk.SystemRequirementsChecker;
-
-import static org.altbeacon.beacon.BeaconParser.ALTBEACON_LAYOUT; // m:2-3=beac, i:4-19,i:20-21,i:22-23,p:24-24,d:25-25
+import java.util.Map;
+import java.util.SimpleTimeZone;
 
 public class beacon extends AppCompatActivity implements BeaconConsumer {
 
-    private String TAG = MainActivity.class.getSimpleName();
+    private final String TAG = MainActivity.class.getSimpleName();
     private BeaconManager beaconManager;
-    private Region beaconRegion = null;
-    // 감지된 비콘들을 임시로 담을 리스트
-    private List<Beacon> beaconList = new ArrayList<>();
-    TextView textView;
 
-    // 탐색 버튼
-    private Button beacon_Button;
+
+    ListView list_beacon;
+    TextView textView;
+    SimpleAdapter beacon_adapter;
+    // 감지된 비콘들을 임시로 담을 리스트
+    private final List<Beacon> beaconList = new ArrayList<>();
+    List<Map<String,String>> beaconlist;
+
+    private static final int PERMISSION_REQUEST_FINE_LOCATION = 1;
+    private static final int PERMISSION_REQUEST_BACKGROUND_LOCATION = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beacon);
-
+        list_beacon= findViewById(R.id.list_beacon);
+        textView = findViewById(R.id.textView);
         SystemRequirementsChecker.checkWithDefaultDialogs(this);
-        textView = findViewById(R.id.tvId);
 
+        beaconlist = new ArrayList<>();
+        beacon_adapter = new SimpleAdapter(this, beaconlist, android.R.layout.simple_list_item_2, new String[]{"UUID","distance"}, new int[]{android.R.id.text1, android.R.id.text2});
+        list_beacon.setAdapter(beacon_adapter);
 
         // 비콘 탐지를 위한 비콘 매니저 객체 초기화
         beaconManager = BeaconManager.getInstanceForApplication(this);
-        // setBeaconLayout 설정 ALTBEACON_LAYOUT
-        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(ALTBEACON_LAYOUT));
+        // setBeaconLayout 설정 Alt beacon_LAYOUT
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
 
         // 비콘 탐지 시작 (where actual service starts)
         beaconManager.bind(this);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        Log.d(TAG, "onBeaconServiceConnect starts");
+
+        beaconManager.removeAllMonitorNotifiers();
+        beaconManager.addRangeNotifier((beacons, region) -> {
+            textView.setText("비콘 찾음!");
+            if(beacons.size() > 0){
+                beaconList.clear();
+                beaconList.addAll(beacons);
+            }
+        });
+
+        try{
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch(RemoteException ignored){ }
+    }
+
+    // 버튼 누를시 탐색된 비콘의 UUID, Distance 출력
+    public void onClicked(View view){
+
+        beaconlist.clear();
+        for (Beacon beacon : beaconList) {
+            Map map = new HashMap();
+            map.put("UUID", ("UUID : " + beacon.getId1())); // beacon UUID
+            map.put("distance",("Distance : " + Double.parseDouble(String.format("%.3f", beacon.getDistance())) + " m"));
+            beaconlist.add(map);
+            beacon_adapter.notifyDataSetChanged();
+        }
+
     }
 
     @Override
@@ -68,42 +115,4 @@ public class beacon extends AppCompatActivity implements BeaconConsumer {
         beaconManager.unbind(this);
     }
 
-    @Override
-    public void onBeaconServiceConnect() {
-        Log.d(TAG, "onBeaconServiceConnect starts");
-
-        beaconManager.removeAllMonitorNotifiers();
-        beaconManager.addRangeNotifier(new RangeNotifier() {
-            @Override
-            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-                if(beacons.size() > 0){
-                    beaconList.clear();
-                    for(Beacon beacon : beacons){
-                        beaconList.add(beacon);
-                    }
-                }
-            }
-        });
-
-        try{
-            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
-        } catch(RemoteException e){ }
-    }
-
-    public void onClicked(View view){
-        handler.sendEmptyMessage(0);
-    }
-
-    Handler handler = new Handler(Looper.getMainLooper()){
-        public void handleMessage(Message msg){
-            textView.setText("");
-
-        for(Beacon beacon : beaconList){
-            textView.append("ID : " + beacon.getId2() + "// Distance" + Double.parseDouble(String.format("%.3f", beacon.getDistance())) + "m\n");
-        }
-        handler.sendEmptyMessageDelayed(0, 1000);
-        }
-
-
-    };
 }
